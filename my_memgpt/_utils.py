e565d1cac043678d2d3df825b25071d297b21443
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-07-07 18:19:27 +0300

    added vectorstore

--- my_memgpt/_utils.py
+++ my_memgpt/_utils.py
@@ -11,3 +11,31 @@ from langchain_postgres.vectorstores import PGVector
 from my_memgpt import _schemas as schemas
 from my_memgpt import _settings as settings
 
+_DEFAULT_DELAY = 60  # seconds
+
+@langsmith.traceable
+def ensure_configurable(config: RunnableConfig) -> schemas.GraphConfig:
+    """Merge the user-provided config with default values."""
+    configurable = config.get("configurable", {})
+    return {
+        **configurable,
+        **schemas.GraphConfig(
+            delay=configurable.get("delay", _DEFAULT_DELAY),
+            model=configurable.get("model", settings.SETTINGS.model),
+            thread_id=configurable["thread_id"],
+            user_id=configurable["user_id"],
+        ),
+    }
+
+@lru_cache
+def get_vectorstore():
+    embeddings = HuggingFaceEmbeddings(model_name=settings.SETTINGS.embeddings_model, model_kwargs={"device":settings.SETTINGS.device})
+    vectorstore = PGVector(
+        embeddings=embeddings,
+        collection_name=settings.SETTINGS.pg_collection_name,
+        connection=settings.SETTINGS.pg_connection,
+        use_jsonb=True,
+    )
+    return vectorstore
+
+__all__ = ["ensure_configurable"]
